4c3c3ec30f1a36a315c599ee9c9234adc7a3a5cc
Splantic <38259041+Splantic@users.noreply.github.com>
2019-06-23 10:59:12 +0200

    Add files via upload

---
+++ Aplikacija/Aplikacija/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Aplikacija
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string korisnicko_ime = Environment.UserName;
+            string[] popis_datoteka = Directory.GetFiles(@"C:\Users\" + korisnicko_ime + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default");
+
+            string putanja_chrome = String.Format("C:\\Users\\{0}\\AppData\\Local\\Google\\Chrome\\User Data\\Default", korisnicko_ime);
+
+            Console.WriteLine(putanja_chrome);
+            Console.ReadKey();
+            Environment.Exit(-1);
+
+            string putanja_IE;
+            string putanja_firefox;
+
+            string naredba = Console.ReadLine();
+
+            if(naredba == "/izbrisi_chrome")
+            {
+                if (Directory.Exists(""));
+            }
+
+            foreach(string trenutna_datoteka in popis_datoteka)
+            {
+                Console.WriteLine("Brisem: {0}", trenutna_datoteka);
+                System.Threading.Thread.Sleep(50);
+                File.Delete(trenutna_datoteka);
+
+                Console.WriteLine("Brisanje zavrseno.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
